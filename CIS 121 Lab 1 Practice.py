740ae8189b7ababab83be5be4d0585ea2fac1ec6
nebil.yousuf@mnsu.edu <nebil.yousuf@mnsu.edu>
2022-09-01 18:51:44 -0500

    CIS 121 Lab 1

---
+++ CIS 121 Lab 1 Practice.py
@@ -0,0 +1,15 @@
+''''''
+#Human_age = int(input("Enter the human age:"))
+#Dog_years = Human_age * 7
+#print("human age at",Human_age,"should be",Dog_years,"in dog yeras")
+
+#Create a script
+#name = str(input("Enter name:"))
+#Last_name = str(input("Enter last name:"))
+#age = int(input("Enter age:"))
+
+#print("hi",name,Last_name,"you are",age,"years old,","","damn")
+
+age = int(input("ente the age:"))
+height = age ** 2
+print("human age of",age,"has an estimated height of",height)
